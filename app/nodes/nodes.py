83f0040e72a3c501e5f2dc9efa4f32938f1a411e
Ruben Giuriato <r.giuriato@hostinato.it>
2026-03-16 11:56:17 +0100

    Creato nodo esecutore del piano. Aggiunta prima azione del piano con reason e campo result. Dettagliato meglio la scelta delle abilità con degli esempi.

--- app/nodes/nodes.py
+++ app/nodes/nodes.py
@@ -1,7 +1,7 @@
 from app.prompts import SYSTEM_PROMPT, PLANNER_PROMPT
 from app.types.planning import Plan
 from app.types.state import State
-from app.tools import make_player_tools, tools_list
+from app.tools import make_player_tools
 from langchain.agents import create_agent
 from langchain.chat_models import init_chat_model
 from typing import cast, Any
@@ -18,9 +18,14 @@ def send_message(state: State) -> dict:
 def planner(state: State) -> dict:	
 	agent = create_agent(
 		model=init_chat_model(model='qwen3.5:397b-cloud', model_provider='ollama'),
-		#tools=tools_list + make_player_tools(state),
 		system_prompt=PLANNER_PROMPT,
 		response_format=Plan
 	)
 	response = agent.invoke(cast(Any, {'messages': state['messages']}))
 	return {'plan': response['structured_response']}
+
+def executor(state: State) -> dict:
+	if len(state['plan'].actions) == 0:
+		return {}
+	# TODO Eseguire le azioni e memorizzare i risultati
+	return {'plan': state['plan']}
--- app/types/planning.py
+++ app/types/planning.py
@@ -1,10 +1,25 @@
+from app.entities.features import AbilityType
 from pydantic import BaseModel, Field
 from typing import Literal
 
-class ThrowDiceAction(BaseModel):
-	action: Literal['throw_dice'] = Field(description='Action to throw a dice', default='throw_dice')
-	dice: Literal[4, 6, 8, 10, 12, 20, 100] = Field(description='The dice to throw')
+
+class BaseAction(BaseModel):
+	action: str = Field(description='The action to perform')
 	reason: str = Field(description='The reason why this action is necessary')
+	result: str = Field(description='The result of the action', default='')
+
+class AbilityCheckAction(BaseAction):
+	action: Literal['ability_check'] = Field(description='''Action to perform an ability check.
+EXAMPLES OF CHECKS:
+Strength: Lift, push, pull, or break something
+Dexterity: Move nimbly, quickly, or quietly
+Constitution: Push your body beyond normal limits
+Intelligence: Reason or remember
+Wisdom: Notice things in the environment or in creatures’ behavior
+Charisma: Influence, entertain, or deceive''', default='ability_check')
+	ability: AbilityType = Field(description='The ability to check against')
+	difficulty_class: int = Field(description='The difficulty class to equal or exceed to succeed')
+
 
 class Plan(BaseModel):
-	actions: list[ThrowDiceAction] = Field(description='A list of actions to execute.', default_factory=list)
+	actions: list[AbilityCheckAction] = Field(description='A list of actions to execute.', default_factory=list)

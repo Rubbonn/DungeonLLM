a070e2da285e0b00830eb267c0d69d39f53815e6
Ruben Giuriato <r.giuriato@hostinato.it>
2026-03-26 13:15:21 +0100

    Spostati i parser dell'SRD in un file dedicato.

--- app/graph/srdparse.py
+++ app/graph/srdparse.py
@@ -1,12 +1,12 @@
-from app.nodes import nodes
+from app.nodes import nodes, parsers
 from app.types.state import SrdParserState
 from langgraph.graph import StateGraph, START, END
 
 def build_graph():
 	graph_builder = StateGraph(SrdParserState)
 	graph_builder.add_node('srd_splitter', nodes.srd_splitter)
-	graph_builder.add_node('gear_parser', nodes.gear_parser)
-	graph_builder.add_node('weapons_parser', nodes.weapons_parser)
+	graph_builder.add_node('gear_parser', parsers.gear_parser)
+	graph_builder.add_node('weapons_parser', parsers.weapons_parser)
 	graph_builder.add_edge(START, 'srd_splitter')
 	graph_builder.add_edge('srd_splitter', 'gear_parser')
 	graph_builder.add_edge('gear_parser', 'weapons_parser')

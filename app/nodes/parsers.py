8ec04cc211a00792aa1ddf8835ac63b711d62f0e
Ruben Giuriato <gujojo@hotmail.it>
2026-04-04 12:37:06 +0200

    Uniformato i parser con la stessa struttura.

--- app/nodes/parsers.py
+++ app/nodes/parsers.py
@@ -7,28 +7,29 @@ import re
 def gear_parser(state: SrdParserState) -> dict:
 	print(f"Parsing gear items...")
 	from app.entities.items import Gear
-	if not 'Equipment' in state['sections'] or not Gear.is_empty():
+	if not 'Equipment' in state['sections']:
 		return {}
 
-	print(f"Extracting gear items...")
-	extraction_regex = re.compile(r'^##(?!#)\s*Adventuring Gear\s*\n([\s\S]*?)(?=\n##(?!#)\s*|\Z)', re.MULTILINE)
-	with open('data/temp/Equipment.md', 'r', encoding='utf-8') as source:
-		match = extraction_regex.search(source.read())
-	if not match:
-		return {}
+	if Gear.is_empty():
+		print(f"Extracting gear items...")
+		extraction_regex = re.compile(r'^##(?!#)\s*Adventuring Gear\s*\n([\s\S]*?)(?=\n##(?!#)\s*|\Z)', re.MULTILINE)
+		with open('data/temp/Equipment.md', 'r', encoding='utf-8') as source:
+			match = extraction_regex.search(source.read())
+		if not match:
+			return {}
 
-	llm = get_chat_model().with_structured_output(GearItems)
-	response: GearItems = retry_exception(func=llm.invoke, input=f'''Extract the gear items from the following text, providing their name, weight, cost and description:
-	{match.group(1)}
-	''')
-	if len(response.items) == 0:
-		raise Exception('No gear items found in the text')
+		llm = get_chat_model().with_structured_output(GearItems)
+		response: GearItems = retry_exception(func=llm.invoke, input=f'''Extract the gear items from the following text, providing their name, weight, cost and description:
+		{match.group(1)}
+		''')
+		if len(response.items) == 0:
+			raise Exception('No gear items found in the text')
 
-	session = get_database_session()
-	for item in response.items:
-		gear_item = Gear(name=item.name, weight=item.weight, cost=item.cost, description=item.description)
-		session.add(gear_item)
-	session.commit()
+		session = get_database_session()
+		for item in response.items:
+			gear_item = Gear(name=item.name, weight=item.weight, cost=item.cost, description=item.description)
+			session.add(gear_item)
+		session.commit()
 	print("Gear items parsing completed successfully.")
 	return {}
 
@@ -37,26 +38,26 @@ def weapons_parser(state: SrdParserState) -> dict:
 		return {}
 
 	print(f"Parsing weapons...")
-	llm = get_chat_model()
-	session = get_database_session()
-
-	extraction_regex = re.compile(r'^##(?!#)\s*Weapons\s*\n([\s\S]*?)(?=\n##(?!#)\s*|\Z)', re.MULTILINE)
-	with open('data/temp/Equipment.md', 'r', encoding='utf-8') as source:
-		match = extraction_regex.search(source.read())
-	if not match:
-		return {}
 
 	# Weapons extraction
 	from app.entities.items import Weapon, WeaponProperty
 	if Weapon.is_empty():
 		print("Extracting weapons...")
-		llm_with_structured_output = llm.with_structured_output(Weapons)
+
+		extraction_regex = re.compile(r'^##(?!#)\s*Weapons\s*\n([\s\S]*?)(?=\n##(?!#)\s*|\Z)', re.MULTILINE)
+		with open('data/temp/Equipment.md', 'r', encoding='utf-8') as source:
+			match = extraction_regex.search(source.read())
+		if not match:
+			return {}
+	
+		llm_with_structured_output = get_chat_model().with_structured_output(Weapons)
 		response: Weapons = retry_exception(func=llm_with_structured_output.invoke, input=f'''Extract the weapons from the following text, providing their name, description, damage, damage type, properties, mastery property, weight, and cost:
 			{match.group(1)}
 			''')
 		if len(response.items) == 0:
 			raise Exception('No weapons found in the text')
 
+		session = get_database_session()
 		for item in response.items:
 			weapon = Weapon(name=item.name, damage=item.damage, damage_type=item.damage_type, versatile_damage=item.versatile_damage,
 			                properties={WeaponProperty(property=property) for property in item.properties}, mastery_property=item.mastery_property,
@@ -75,31 +76,30 @@ def tools_parser(state: SrdParserState) -> dict:
 	print('Parsing tools...')
 
 	from app.entities.items import Tool
-	if not Tool.is_empty():
-		return {}
+	if Tool.is_empty():
+		print('Extracting tools...')
 
-	print('Extracting tools...')
-	extraction_regex = re.compile(r'^##(?!#)\s*Tools\s*\n([\s\S]*?)(?=\n##(?!#)\s*|\Z)', re.MULTILINE)
-	with open('data/temp/Equipment.md', 'r', encoding='utf-8') as source:
-		match = extraction_regex.search(source.read())
-	if not match:
-		return {}
+		extraction_regex = re.compile(r'^##(?!#)\s*Tools\s*\n([\s\S]*?)(?=\n##(?!#)\s*|\Z)', re.MULTILINE)
+		with open('data/temp/Equipment.md', 'r', encoding='utf-8') as source:
+			match = extraction_regex.search(source.read())
+		if not match:
+			return {}
 
-	llm = get_chat_model().with_structured_output(Tools)
-	response: Tools = retry_exception(func=llm.invoke, input=f'''Extract the tools from the following text, providing their properties (extract any variant as a separate tool):
-	{match.group(1)}
-	''')
-	if len(response.items) == 0:
-		raise Exception('No tools found in the text')
+		llm = get_chat_model().with_structured_output(Tools)
+		response: Tools = retry_exception(func=llm.invoke, input=f'''Extract the tools from the following text, providing their properties (extract any variant as a separate tool):
+		{match.group(1)}
+		''')
+		if len(response.items) == 0:
+			raise Exception('No tools found in the text')
 
-	session = get_database_session()
-	from app.entities.items import Gear
-	from sqlalchemy import select
-	for item in response.items:
-		gear_crafted = session.scalars(select(Gear).where(Gear.name.in_([gear.name for gear in item.craft]))).all()
-		tool = Tool(name=item.name, weight=item.weight, cost=item.cost, ability=item.ability, utilize=item.utilize, craft=gear_crafted)
-		session.add(tool)
-	session.commit()
+		session = get_database_session()
+		from app.entities.items import Gear
+		from sqlalchemy import select
+		for item in response.items:
+			gear_crafted = session.scalars(select(Gear).where(Gear.name.in_([gear.name for gear in item.craft]))).all()
+			tool = Tool(name=item.name, weight=item.weight, cost=item.cost, ability=item.ability, utilize=item.utilize, craft=gear_crafted)
+			session.add(tool)
+		session.commit()
 	print("Tools parsing completed successfully.")
 	return {}
 
@@ -109,15 +109,13 @@ def animals_parser(state: SrdParserState) -> dict:
 
 	print('Parsing animals...')
 	from app.entities.creatures import Animal
-	if not Animal.is_empty():
-		return {}
-
-	print('Extracting animals...')
-	from app.types.models import Animals
-	llm = get_chat_model().with_structured_output(Animals)
-	with open('data/temp/Animals.md', 'r', encoding='utf-8') as source:
-		content = source.read()
-	response: Animals = retry_exception(func=llm.invoke, input=f'''Extract all animals from the following text and populate each animal's fields according to these rules:
+	if Animal.is_empty():
+		print('Extracting animals...')
+		from app.types.models import Animals
+		llm = get_chat_model().with_structured_output(Animals)
+		with open('data/temp/Animals.md', 'r', encoding='utf-8') as source:
+			content = source.read()
+		response: Animals = retry_exception(func=llm.invoke, input=f'''Extract all animals from the following text and populate each animal's fields according to these rules:
 
 - name: the heading name (e.g. "Allosaurus")
 - size: one of Tiny, Small, Medium, Large, Huge, Gargantuan — from the italic line (e.g. "*Large Beast (Dinosaur), Unaligned*")
@@ -142,34 +140,34 @@ def animals_parser(state: SrdParserState) -> dict:
 Text:
 {content}
 ''')
-	if len(response.items) == 0:
-		raise Exception('No animals found in the text')
+		if len(response.items) == 0:
+			raise Exception('No animals found in the text')
 
-	session = get_database_session()
-	from app.entities.creatures import Creature, CreatureAbility, CreatureTrait, SkillProficiencies, Language, CreatureSpeed
-	for animal in response.items:
-		a = Animal(
-			creature=Creature(
-				name=animal.name,
-			    size=animal.size,
-			    abilities={ability: CreatureAbility(ability=ability, value=value, modifier=modifier, save_modifier=save_modifier) for ability, (value, modifier, save_modifier) in animal.abilities.items()},
-				armor_class=animal.armor_class,
-				hit_points=animal.hit_points,
-				skill_proficiencies=[SkillProficiencies(skill=skill, bonus=bonus) for skill, bonus in animal.skill_proficiencies.items()],
-				languages=[Language(language=language) for language in animal.languages],
-				alignment=animal.alignment,
-				speed={speed_type: CreatureSpeed(speed_type=speed_type, speed=speed.speed, conditions=speed.conditions) for speed_type, speed in animal.speed.items()},
-				traits=[CreatureTrait(name=name, description=description) for name, description in animal.traits.items()]
-			),
-			creature_type=animal.creature_type,
-			creature_sub_type=animal.creature_sub_type,
-			hit_points_formula=animal.hit_points_formula,
-			experience_points=animal.experience_points,
-			initiative_bonus=animal.initiative_bonus,
-			challenge_rating=animal.challenge_rating,
-		)
-		session.add(a)
-	session.commit()
+		session = get_database_session()
+		from app.entities.creatures import Creature, CreatureAbility, CreatureTrait, SkillProficiencies, Language, CreatureSpeed
+		for animal in response.items:
+			a = Animal(
+				creature=Creature(
+					name=animal.name,
+					size=animal.size,
+					abilities={ability: CreatureAbility(ability=ability, value=value, modifier=modifier, save_modifier=save_modifier) for ability, (value, modifier, save_modifier) in animal.abilities.items()},
+					armor_class=animal.armor_class,
+					hit_points=animal.hit_points,
+					skill_proficiencies=[SkillProficiencies(skill=skill, bonus=bonus) for skill, bonus in animal.skill_proficiencies.items()],
+					languages=[Language(language=language) for language in animal.languages],
+					alignment=animal.alignment,
+					speed={speed_type: CreatureSpeed(speed_type=speed_type, speed=speed.speed, conditions=speed.conditions) for speed_type, speed in animal.speed.items()},
+					traits=[CreatureTrait(name=name, description=description) for name, description in animal.traits.items()]
+				),
+				creature_type=animal.creature_type,
+				creature_sub_type=animal.creature_sub_type,
+				hit_points_formula=animal.hit_points_formula,
+				experience_points=animal.experience_points,
+				initiative_bonus=animal.initiative_bonus,
+				challenge_rating=animal.challenge_rating,
+			)
+			session.add(a)
+		session.commit()
 	print('Animals parsing completed successfully.')
 	return {}
 
